4d9bf71427a688edc91539a28f76586fae35099b
duanbm1911 <75944711+duanbm1911@users.noreply.github.com>
2023-10-24 15:18:25 +0700

    Update dashboard-02.js

--- static/inventory/js/dashboard-02.js
+++ static/inventory/js/dashboard-02.js
@@ -1,32 +1,31 @@
-window.onload = function() {
-	var chart = new CanvasJS.Chart("chartContainer-02", {
-		animationEnabled: true,
-		theme: "light2", // "light1", "light2", "dark1", "dark2"
-		exportEnabled: true,
-		title: {
-			text: "Count device by vendor"
-		},
-		axisY: {
-			title: "Device count",
-			suffix: ""
-		},
-		axisX: {
-			title: ""
-		},
-		data: [{
-			type: "column",
-			yValueFormatString: "",
-			dataPoints: [
-				{ label: "Cisco", y: 300 },	
-				{ label: "Checkpoint", y: 200 },	
-				{ label: "Fortigate", y: 10 },
-				{ label: "Symantec", y: 10 },	
-				{ label: "F5", y: 5 },
-				{ label: "Citrix", y: 4 }
-			]
-		}]
-	});
-	chart.render();
-};
+
+var chart = new CanvasJS.Chart("chartContainer-02", {
+animationEnabled: true,
+theme: "light2", // "light1", "light2", "dark1", "dark2"
+exportEnabled: true,
+title: {
+	text: "Count device by vendor"
+},
+axisY: {
+	title: "Device count",
+	suffix: ""
+},
+axisX: {
+	title: ""
+},
+data: [{
+	type: "column",
+	yValueFormatString: "",
+	dataPoints: [
+		{ label: "Cisco", y: 300 },	
+		{ label: "Checkpoint", y: 200 },	
+		{ label: "Fortigate", y: 10 },
+		{ label: "Symantec", y: 10 },	
+		{ label: "F5", y: 5 },
+		{ label: "Citrix", y: 4 }
+	]
+}]
+});
+chart.render();
 
 
