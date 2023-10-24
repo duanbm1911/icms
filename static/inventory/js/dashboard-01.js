a825d84ac8ae91d3ba888cf7ce625c21994a8a89
duanbm1911 <75944711+duanbm1911@users.noreply.github.com>
2023-10-24 15:06:26 +0700

    Update dashboard-01.js

--- static/inventory/js/dashboard-01.js
+++ static/inventory/js/dashboard-01.js
@@ -1,28 +1,32 @@
-var chart = new CanvasJS.Chart("chartContainer-01", {
-	animationEnabled: true,
-	theme: "light2", // "light1", "light2", "dark1", "dark2"
-	exportEnabled: true,
-	title: {
-		text: "Count device by category"
-	},
-	axisY: {
-		title: "Device count",
-		suffix: ""
-	},
-	axisX: {
-		title: ""
-	},
-	data: [{
-		type: "column",
-		yValueFormatString: "",
-		dataPoints: [
-			{ label: "Switch", y: 100 },	
-			{ label: "Router", y: 200 },	
-			{ label: "Firewall", y: 30 },
-			{ label: "Proxy", y: 10 },	
-			{ label: "WAF", y: 5 },
-			{ label: "ADC", y: 4 }
-		]
-	}]
-});
-chart.render();
+window.onload = function() {
+	var chart = new CanvasJS.Chart("chartContainer-01", {
+		animationEnabled: true,
+		theme: "light2", // "light1", "light2", "dark1", "dark2"
+		exportEnabled: true,
+		title: {
+			text: "Count device by category"
+		},
+		axisY: {
+			title: "Device count",
+			suffix: ""
+		},
+		axisX: {
+			title: ""
+		},
+		data: [{
+			type: "column",
+			yValueFormatString: "",
+			dataPoints: [
+				{ label: "Switch", y: 100 },	
+				{ label: "Router", y: 200 },	
+				{ label: "Firewall", y: 30 },
+				{ label: "Proxy", y: 10 },	
+				{ label: "WAF", y: 5 },
+				{ label: "ADC", y: 4 }
+			]
+		}]
+	});
+	chart.render();
+}
+
+
