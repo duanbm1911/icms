fb1349a307f448890f59453309a6b57d09177417
duanbm1911 <75944711+duanbm1911@users.noreply.github.com>
2023-10-24 11:31:03 +0700

    Update dashboard-03.js

--- static/inventory/js/dashboard-03.js
+++ static/inventory/js/dashboard-03.js
@@ -5,22 +5,23 @@ var chart = new CanvasJS.Chart("chartContainer-03", {
 		text: "Count of error log"
 	},
 	subtitles: [{
-		text: "United Kingdom, 2016",
-		fontSize: 16
+		//text: "United Kingdom, 2016",
+		//fontSize: 16
 	}],
 	data: [{
-		type: "pie",
+		type: "line",
 		indexLabelFontSize: 18,
 		radius: 80,
 		indexLabel: "{label} - {y}",
 		yValueFormatString: "###0.0\"%\"",
 		click: explodePie,
 		dataPoints: [
-			{ y: 42, label: "Gas" },
-			{ y: 21, label: "Nuclear"},
-			{ y: 24.5, label: "Renewable" },
-			{ y: 9, label: "Coal" },
-			{ y: 3.1, label: "Other Fuels" }
+			{ y: 42, label: "DC/DR" },
+			{ y: 21, label: "Branch"},
+			{ y: 24.5, label: "HO-89LH" },
+			{ y: 9, label: "HO-VHT" },
+			{ y: 3.1, label: "HO-VAT" },
+			{ y: 3.1, label: "HO-MNT" }
 		]
 	}]
 });
