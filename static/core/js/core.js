2d65d1afc2e826329c0c15d2d6dba086c9662a04
duanbm1911 <75944711+duanbm1911@users.noreply.github.com>
2023-10-25 13:59:59 +0700

    Update core.js

--- static/core/js/core.js
+++ static/core/js/core.js
@@ -1,11 +1,5 @@
 jQuery(document).ready(function($){
-    $('li.dropdown-submenu-inventory a[data-toggle="dropdown"]').on('click', function(event) {
-      event.preventDefault();
-      event.stopPropagation();
-      $('li.dropdown-submenu').not($(this).parent()).removeClass('open');
-      $(this).parent().toggleClass('open');
-    });
-    $('li.dropdown-submenu-ipplan a[data-toggle="dropdown"]').on('click', function(event) {
+    $('li.dropdown-submenu a[data-toggle="dropdown"]').on('click', each.function(event) {
       event.preventDefault();
       event.stopPropagation();
       $('li.dropdown-submenu').not($(this).parent()).removeClass('open');
