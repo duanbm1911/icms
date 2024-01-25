a6df5500a63af1c3abe6ac0b0b9cc3eb2cd98830
duanbm1911 <75944711+duanbm1911@users.noreply.github.com>
2024-01-25 04:52:02 +0000

    update

--- icms/settings.py
+++ icms/settings.py
@@ -80,13 +80,23 @@ WSGI_APPLICATION = 'icms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ICMS',
+        'USER': 'root',
+        'PASSWORD': 'qEnN5P#W6XdRRr+2',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
