acf9955d7b31df10a3d46f8e499ccc8caf10951a
duanbm1911 <75944711+duanbm1911@users.noreply.github.com>
2024-02-19 16:17:23 +0000

    update

--- inventory/models.py
+++ inventory/models.py
@@ -113,14 +113,15 @@ class DeviceTopology(models.Model):
     def __str__(self):
         """Unicode representation of DeviceTopology."""
         return str(self.device_ip)
+    
 
 class DeviceConfiguration(models.Model):
     """Model definition for DeviceConfiguration."""
 
     device_ip = models.ForeignKey('DeviceBasicInfo', on_delete=models.CASCADE)
-    device_config_standardized = models.CharField(max_length=100, blank=True, null=True)
-    device_monitored = models.CharField(max_length=100, blank=True, null=True)
-    device_backup_config = models.CharField(max_length=100, blank=True, null=True)
+    device_config_standardized = models.BooleanField(blank=True, null=True)
+    device_monitored = models.BooleanField(blank=True, null=True)
+    device_backup_config = models.BooleanField(blank=True, null=True)
     user_created = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
