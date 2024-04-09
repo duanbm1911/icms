8fe2c964fb62668fc48458af0738e3f8890e7a99
duanbm1911 <duanbm1911@gmail.com>
2024-04-09 03:09:04 +0000

    update

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from cm.models import *
+from inventory.models import *
+from ipplan.models import *
 
 # Register your models here.
+
+from django.contrib import admin
+
+
+class MyAdminSite(admin.AdminSite):
+    site_header = "ICMS ADMINISTRATOR"
+    
+    
+admin_site = MyAdminSite(name="ICMS_ADMIN")
+admin_site.register(DeviceLocation)
+admin_site.register(DeviceType)
+admin_site.register(DeviceCategory)
+admin_site.register(DeviceVendor)
+admin_site.register(DeviceOS)
+admin_site.register(Device)
+admin_site.register(DeviceManagement)
+admin_site.register(DeviceInterface)
+admin_site.register(DeviceTopology)
+admin_site.register(DeviceConfiguration)
+admin_site.register(DeviceFirmware)
+
+admin_site.register(CheckpointRule)
+admin_site.register(CheckpointSite)
+admin_site.register(CheckpointPolicy)
+
+admin_site.register(Region)
+admin_site.register(Location)
+admin_site.register(Subnet)
+admin_site.register(IpStatus)
+admin_site.register(IpAddressModel)
--- icms/urls.py
+++ icms/urls.py
@@ -18,10 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from core.admin import admin_site
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('icms-admin/', admin_site.urls),
     path('accounts/', include('accounts.urls')),
     path('inventory/', include('inventory.urls')),
     path('ipplan/', include('ipplan.urls')),
