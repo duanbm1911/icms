089c7ac014473ba71099ba36157a937476f9dd7f
duanbm1911 <duanbm1911@gmail.com>
2024-04-09 03:41:27 +0000

    update

--- icms/settings.py
+++ icms/settings.py
@@ -129,11 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = os.getenv("STATIC_URL")
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_URL = os.getenv("STATIC_URL")
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
-# MEDIA_URL = os.getenv("MEDIA_URL")
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = os.getenv("MEDIA_URL")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
