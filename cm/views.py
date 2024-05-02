23164eddd68c3744f7c578a745f27e7effd11191
duanbm1911 <duanbm1911@gmail.com>
2024-05-02 04:24:26 +0000

    update

--- cm/views.py
+++ cm/views.py
@@ -169,7 +169,7 @@ class CheckpointRuleListView(ListView):
         context = {
             'list_rule_created': CheckpointRule.objects.filter(Q(status='Created') | Q(status='Install-Only')),
             'list_rule_process': CheckpointRule.objects.filter(status='Processing'),
-            'list_rule_success': CheckpointRule.objects.filter(status='Success').order_by('-id')[:500],
+            'list_rule_success': CheckpointRule.objects.filter(status='Success').order_by('-id'),
             'list_rule_failed': CheckpointRule.objects.filter(status='Failed'),
         }
         return context
