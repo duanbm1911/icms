24c7fcdd933fb2f4bba39397916049b87c17f8d1
duanbm1911 <duanbm1911@gmail.com>
2024-05-30 09:08:04 +0000

    update

--- accounts/forms.py
+++ accounts/forms.py
@@ -30,7 +30,6 @@ class ValidatingPasswordChangeForm(auth.forms.PasswordChangeForm):
             raise forms.ValidationError("The new password must contain at least one upper characters")
 
         symbol_error = re.search(r"[ @!#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password1)
-        print(symbol_error, password1)
         if symbol_error is None:
             raise forms.ValidationError("The new password must contain at least one symbol characters")
         return password1
