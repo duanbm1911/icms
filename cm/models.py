0012ee20e072775ddb85259ecf0ba6faabab2196
duanbm1911 <duanbm1911@gmail.com>
2024-07-19 07:35:08 +0000

    update

--- cm/models.py
+++ cm/models.py
@@ -6,7 +6,7 @@ class CheckpointPolicy(models.Model):
     policy = models.CharField(max_length=200, unique=True)
     layer = models.CharField(max_length=100, blank=True, null=True)
     site = models.ForeignKey('CheckpointSite', on_delete=models.CASCADE)
-    section = models.CharField(max_length=100, blank=True)
+    # section = models.CharField(max_length=100, blank=True)
     
     def __str__(self):
         return self.policy
