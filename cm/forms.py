b2f773a7827529eaf8794466caacc12db5c70ee1
duanbm1911 <duanbm1911@gmail.com>
2024-07-19 08:04:27 +0000

    update

--- cm/forms.py
+++ cm/forms.py
@@ -1,100 +1,100 @@
-# from django import forms
-# from cm.models import *
+from django import forms
+from cm.models import *
 
 
-# class CheckpointPolicyForm(forms.ModelForm):
+class CheckpointPolicyForm(forms.ModelForm):
 
-#     class Meta:
+    class Meta:
 
-#         model = CheckpointPolicy
-#         fields = [
-#             'policy',
-#             'layer',
-#             'site'
-#         ]
+        model = CheckpointPolicy
+        fields = [
+            'policy',
+            'layer',
+            'site'
+        ]
 
-# class CheckpointSiteForm(forms.ModelForm):
+class CheckpointSiteForm(forms.ModelForm):
 
-#     class Meta:
+    class Meta:
 
-#         model = CheckpointSite
-#         fields = [
-#             'site',
-#             'smc'
-#         ]
+        model = CheckpointSite
+        fields = [
+            'site',
+            'smc'
+        ]
 
-# class CheckpointRuleForm(forms.ModelForm):
+class CheckpointRuleForm(forms.ModelForm):
     
-#     STATUS = (
-#         ('Created', 'Created'),
-#         ('Processing', 'Processing'),
-#         ('Failed', 'Failed'),
-#         ('Success', 'Success'),
-#         ('Install-Only', 'Install-Only')
-#     )
-#     section = forms.ChoiceField(choices=CheckpointRuleSection.objects.all())
-#     status = forms.ChoiceField(choices=STATUS)
+    STATUS = (
+        ('Created', 'Created'),
+        ('Processing', 'Processing'),
+        ('Failed', 'Failed'),
+        ('Success', 'Success'),
+        ('Install-Only', 'Install-Only')
+    )
+    section = forms.ChoiceField(choices=CheckpointRuleSection.objects.all())
+    status = forms.ChoiceField(choices=STATUS)
     
-#     class Meta:
-#         model = CheckpointRule
-#         fields = [
-#             'policy',
-#             'description',
-#             'source',
-#             'destination',
-#             'protocol',
-#             'schedule',
-#             # 'section',
-#             'status'
-#         ]
+    class Meta:
+        model = CheckpointRule
+        fields = [
+            'policy',
+            'description',
+            'source',
+            'destination',
+            'protocol',
+            'schedule',
+            # 'section',
+            'status'
+        ]
         
-# class F5DeviceForm(forms.ModelForm):
+class F5DeviceForm(forms.ModelForm):
     
-#     class Meta:
-#         model = F5Device
-#         fields = [
-#             'f5_device_ip',
-#             'f5_device_name',
-#         ]
-        
-
-# class F5CreateVirtualServerForm(forms.ModelForm):
-
-#     class Meta:
-#         model = F5CreateVirtualServer
-#         fields = [
-#             'f5_device_ip',
-#             'f5_template',
-#             'service_name',
-#             'vs_name',
-#             'vs_ip',
-#             'vs_port',
-#             'pool_member',
-#             'pool_monitor',
-#             'pool_lb_method',
-#             'client_ssl_profile',
-#             'server_ssl_profile',
-#             'irules',
-#             'waf_profile'
-#         ]
+    class Meta:
+        model = F5Device
+        fields = [
+            'f5_device_ip',
+            'f5_device_name',
+        ]
         
 
-# class F5TemplateForm(forms.ModelForm):
+class F5CreateVirtualServerForm(forms.ModelForm):
 
-#     class Meta:
-#         model = F5Template
-#         fields = [
-#             'template_name',
-#             'partition',
-#             'protocol',
-#             'client_protocol_profile',
-#             'server_protocol_profile',
-#             'client_http_profile',
-#             'server_http_profile',
-#             'snat_name',
-#             'http_analytics_profile',
-#             'tcp_analytics_profile',
-#             'web_socket_profile',
-#             'http_compression_profile',
-#             'web_acceleration_profile'
-#         ]
+    class Meta:
+        model = F5CreateVirtualServer
+        fields = [
+            'f5_device_ip',
+            'f5_template',
+            'service_name',
+            'vs_name',
+            'vs_ip',
+            'vs_port',
+            'pool_member',
+            'pool_monitor',
+            'pool_lb_method',
+            'client_ssl_profile',
+            'server_ssl_profile',
+            'irules',
+            'waf_profile'
+        ]
+        
+
+class F5TemplateForm(forms.ModelForm):
+
+    class Meta:
+        model = F5Template
+        fields = [
+            'template_name',
+            'partition',
+            'protocol',
+            'client_protocol_profile',
+            'server_protocol_profile',
+            'client_http_profile',
+            'server_http_profile',
+            'snat_name',
+            'http_analytics_profile',
+            'tcp_analytics_profile',
+            'web_socket_profile',
+            'http_compression_profile',
+            'web_acceleration_profile'
+        ]
