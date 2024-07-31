4efbab13b42c79ce7c10527c03b1e857e0ac39c7
duanbm1911 <duanbm1911@gmail.com>
2024-07-31 09:42:27 +0000

    update

--- api/views.py
+++ api/views.py
@@ -350,7 +350,7 @@ def ipplan_dashboard_03(request):
             'y': percent,
             'toolTipContent': f'{replace_characters(subnet)} - {percent}%'
         })
-    datalist = sorted(datalist, key=lambda x: x['y'], reverse=False)[:20]
+    datalist = sorted(datalist, key=lambda x: x['y'], reverse=True)[:20]
     return JsonResponse({'data': datalist})
 
 
